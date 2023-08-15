edf519de8b18c9ffe2c653154c65772d722cb643
developersrinu <132943635+developersrinu@users.noreply.github.com>
2023-08-15 23:06:32 +0000

    changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react'; // Correct import for React
+import ReactDOM from 'react-dom';
+import App from './App';
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root')); // Correct rendering using ReactDOM
